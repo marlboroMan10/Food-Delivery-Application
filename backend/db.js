8bbea141cea0fa06005f6f0db8d9b35244d2cfad
marlboroMan10 <142051923+marlboroMan10@users.noreply.github.com>
2024-03-12 18:57:08 +0530

    Initial Commit

---
+++ backend/db.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+// const mongoDbClient = require("mongodb").MongoClient
+const mongoURI = 'mongodb://localhost:27017/' 
+module.exports = function (callback) {
+    mongoose.connect(mongoURI, { useNewUrlParser: true }, async (err, result) => {
+        // mongoDbClient.connect(mongoURI, { useNewUrlParser: true }, async(err, result) => {
+        if (err) console.log("---" + err)
+        else {
+            // var database =
+            console.log("connected to mongo")
+            const foodCollection = await mongoose.connection.db.collection("food_items");
+            foodCollection.find({}).toArray(async function (err, data) {
+                const categoryCollection = await mongoose.connection.db.collection("Categories");
+                categoryCollection.find({}).toArray(async function (err, Catdata) {
+                    callback(err, data, Catdata);
+
+                })
+            });
+        }
+    })
+};
